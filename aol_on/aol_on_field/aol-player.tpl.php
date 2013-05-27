3c740e8d98783e91dd00505b21f5a55c03a17bd6
Christina Lee <clee@amilink.com>
2013-05-26 23:55:56 -0400

    Add AOL ON Field module

---
+++ aol_on/aol_on_field/aol-player.tpl.php
@@ -0,0 +1,3 @@
+<div class="aol-player" style="margin:0 auto;width:<?php print $width ?>px;height:<?php print $height; ?>px;">
+  <script type="text/javascript" src="http://pshared.5min.com/Scripts/PlayerSeed.js?sid=<?php print $sid; ?>&amp;playList=<?php print $video_id; ?>&amp;<?php print $query; ?>"></script>
+</div>
